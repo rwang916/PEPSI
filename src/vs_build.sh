372e9b4bb41a0a8980effca2df7ab59fd653512a
Robert Wang <rwang916@berkeley.edu>
2019-04-13 15:05:21 -0700

    fixed build script bug

--- src/vs_build.sh
+++ src/vs_build.sh
@@ -3,26 +3,32 @@
 # This is a build script for setting up the appropriate directories/files prior to running PEPSI
 # To run:
 #
-#	bash vs_build.sh
+#       bash vs_build.sh
 #
 
 # Set up working directories
+src_dir=$(pwd)
 data="../data"
 cadd="$data/cadd"
 
 # Download CADD v1.3 annotations
-wget "https://krishna.gs.washington.edu/download/CADD/v1.3/ExAC_r0.3_inclAnno.tsv.gz" -P "$cadd"
-wget "https://krishna.gs.washington.edu/download/CADD/v1.3/ExAC_r0.3_inclAnno.tsv.gz.tbi" -P "$cadd"
+cd "$cadd"
+wget "https://krishna.gs.washington.edu/download/CADD/v1.3/ExAC_r0.3_inclAnno.tsv.gz"
+wget "https://krishna.gs.washington.edu/download/CADD/v1.3/ExAC_r0.3_inclAnno.tsv.gz.tbi"
 
 # Download MaxEntScan
-wget "http://genes.mit.edu/burgelab/maxent/download/fordownload.tar.gz" -P "$data"
-tar -xvf "$data/fordownload.tar.gz"
-rm "$data/fordownload.tar.gz"
-mv "$data/fordownload" "$data/MaxEntScan"
+cd ".."
+wget "http://genes.mit.edu/burgelab/maxent/download/fordownload.tar.gz"
+tar -xvf "fordownload.tar.gz"
+rm "fordownload.tar.gz"
+mv "fordownload" "MaxEntScan"
 
 # Download SVMBP
-wget "https://bitbucket.org/regulatorygenomicsupf/svm-bpfinder/get/727e2d8ea4ad.zip" -P "$data"
-unzip "$data/727e2d8ea4ad.zip"
-rm "$data/727e2d8ea4ad.zip"
-mv "$data/regulatorygenomicsupf-svm-bpfinder-727e2d8ea4ad" "$data/SVMBP"
-wget "http://regulatorygenomics.upf.edu/Software/SVM_BP/calculate_best_BP_per_intron.pl" -P "$data/SVMBP"
+wget "https://bitbucket.org/regulatorygenomicsupf/svm-bpfinder/get/727e2d8ea4ad.zip"
+unzip "727e2d8ea4ad.zip"
+rm "727e2d8ea4ad.zip"
+mv "regulatorygenomicsupf-svm-bpfinder-727e2d8ea4ad" "SVMBP"
+cd "SVMBP"
+wget "http://regulatorygenomics.upf.edu/Software/SVM_BP/calculate_best_BP_per_intron.pl"
+
+cd "$src_dir"
